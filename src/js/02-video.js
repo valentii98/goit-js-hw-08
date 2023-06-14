539fc20b728c1dad9009468e97a5d2395aea381c
Bohdan Valentii <b.valentii98@gmail.com>
2023-06-14 16:11:30 +0300

    video.js task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+setCurrentTime();
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
