7889bfe9890b4248193332c7260313619ac94d89
Bohdan Valentii <b.valentii98@gmail.com>
2023-06-17 00:01:29 +0300

    add feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,32 @@
-console.log('script done');
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+onFormCreate();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormCreate() {
+  console.log(localStorage.getItem(STORAGE_KEY));
+  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (formData) {
+    email.value = formData.email;
+    message.value = formData.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
